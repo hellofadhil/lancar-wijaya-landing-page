a87840570a85ef764eba03e74613aea125a13057
hellofadhil <hellofadhilr@gmail.com>
2026-05-15 06:49:26 +0700

    final

--- app/layout.tsx
+++ app/layout.tsx
@@ -72,10 +72,19 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: '/placeholder-logo.png',
+        url: '/icon-light-32x32.png',
         media: '(prefers-color-scheme: light)',
       },
-    ]
+      {
+        url: '/icon-dark-32x32.png',
+        media: '(prefers-color-scheme: dark)',
+      },
+      {
+        url: '/icon.svg',
+        type: 'image/svg+xml',
+      },
+    ],
+    apple: '/apple-icon.png',
   },
 }
 
